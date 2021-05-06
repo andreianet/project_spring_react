41c2bcbe366e15e46e2d2d4a31bba07b7293d004
andreianet <andreia.netsys@hotmail.com>
2021-05-06 20:38:38 -0300

    Layers

---
+++ backend/src/main/java/com/andreia/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.andreia.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.andreia.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
---
+++ backend/src/main/java/com/andreia/dsvendas/services/SellerService.java
@@ -0,0 +1,24 @@
+package com.andreia.dsvendas.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andreia.dsvendas.dto.SellerDTO;
+import com.andreia.dsvendas.entities.Seller;
+import com.andreia.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll(){
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+
+}
