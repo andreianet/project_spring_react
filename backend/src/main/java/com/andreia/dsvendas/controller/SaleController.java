90fe5f9ce9c4f14855bbcb640974889f9d4691e6
andreianet <andreia.netsys@hotmail.com>
2021-05-06 22:43:18 -0300

    Pagination

---
+++ backend/src/main/java/com/andreia/dsvendas/controller/SaleController.java
@@ -0,0 +1,32 @@
+package com.andreia.dsvendas.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.andreia.dsvendas.dto.SaleDTO;
+import com.andreia.dsvendas.dto.SellerDTO;
+import com.andreia.dsvendas.services.SaleService;
+import com.andreia.dsvendas.services.SellerService;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+	
+	@Autowired
+	private SaleService service;
+	
+	//test:http://localhost:8080/sales?page=1&size=10&sort=date,desc
+	@GetMapping
+	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable){
+		Page<SaleDTO> list =  service.findAll(pageable);
+		return ResponseEntity.ok(list);
+	}
+
+}
