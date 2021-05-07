32919b6322d637419dd01a510fd0857cd1768a1a
andreianet <andreia.netsys@hotmail.com>
2021-05-07 18:52:07 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,25 @@
+import Dashboard from 'pages/Dashboard';
+import Home from 'pages/Home';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+function Routes() {
+    return (
+      <BrowserRouter>
+      <Switch>
+          <Route path= "/" exact>
+            <Home />
+
+          </Route>
+
+          <Route path= "/dashboard">
+            <Dashboard />
+            
+          </Route>
+      </Switch>
+      </BrowserRouter>
+    );
+  }
+  
+  export default Routes;
+
+  //npm i --save-dev @types/react-router-dom
