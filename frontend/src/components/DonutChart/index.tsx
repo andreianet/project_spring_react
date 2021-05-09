bf5aecadfaaf5c8bfb98e7d9e7050ef7a0c0be3e
andreianet <andreia.netsys@hotmail.com>
2021-05-09 09:12:32 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import Chart from "react-apexcharts";
 import { SaleSum } from "types/sale";
 import { BASE_URL } from "util/request";
@@ -9,20 +10,24 @@ type ChartData =  {
 }
 function DonutChart() {
 
-    //Método errado
-    let chartData: ChartData = {labels: [], series:[]};
+    const [chartData, setChartData] = useState<ChartData>({labels: [], series:[]});
+
+    /*Método errado
+    let chartData: ChartData = {labels: [], series:[]};*/
 
     //requisição para tratar os dados
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
         .then(response => {
             const data = response.data as SaleSum[];
             const myLabels = data.map(x=> x.sellerName);
             const mySeries = data.map(x => x.sum);
 
-            chartData = {labels: myLabels, series: mySeries}
-            console.log(response.data);
+            setChartData({labels: myLabels, series: mySeries})
+            //console.log(chartData);
             
-        });
+        }); 
+    }, []);
     
     //const mockData = {
     //    series: [477138, 499928, 444867, 220426, 473088],
