900ab00b4bcee1383e9e6270faede29970628492
andreianet <andreia.netsys@hotmail.com>
2021-05-09 12:53:23 -0300

    DataTable integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,4 +1,25 @@
-function DataTable() {
+import axios from "axios";
+import { useEffect, useState } from "react";
+import {SalePage} from 'types/sale';
+import { formatLocalDate } from "util/format";
+import { BASE_URL } from "util/request";
+
+const DataTable = () => {
+
+    const [page, setPage] = useState<SalePage>({
+        first: true,
+        last: true,
+        number: 0,
+        totalElements: 0,
+        totalPages: 0
+    })
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales?page=0&size=5&sort=date,desc`)
+            .then(response => {
+                setPage(response.data);
+            });
+    }, []);
   return (
     <div className="table-responsive">
       <table className="table table-striped table-sm">
@@ -12,48 +33,15 @@ function DataTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
+            {page.content?.map(x => (
+                <tr key={x.id}>
+                <th>{formatLocalDate(x.date, "dd/MM/yyyy")}</th>
+                <th>{x.seller.name}</th>
+                <th>{x.visited}</th>
+                <th>{x.deals}</th>
+                <th>{x.amount.toFixed(2)}</th>
+            </tr>
+            ))}
         </tbody>
       </table>
     </div>
